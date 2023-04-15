11a5ebe48ecec72ffd1f96e95e4b076fe92b2482
AlINJFSw <alijanbeincs@gmail.com>
2023-04-15 18:03:51 +0300

    fixed body-parser issue

--- GMarket-server/Controllers/authController.js
+++ GMarket-server/Controllers/authController.js
@@ -7,23 +7,24 @@ exports.sendVerificationCodeSMS = (req,res,next) =>{
 
     const message  = "to verifiy your number this is the verification Code " + verificationCode
     try {
-        const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+        
+        // const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
-        client.messages
-        .create({
-           body: message,
-           from: '+16203776088',
-           to: '+96170838043'
-         })
-        .then(message => {
-            const userExist = User.find({phone_number:'wdwqd'})
-            // if (userExist) {
-            //     console.log(userExist);
-            // }
-            res.send("message sent")}).catch(error =>{
-            const err = new HttpError(error.message,405)
-            next(err);
-        });
+        // client.messages
+        // .create({
+        //    body: message,
+        //    from: '+16203776088',
+        //    to: '+96170838043'
+        //  })
+        // .then(message => {
+        //     const userExist = User.find({phone_number:'wdwqd'})
+        //     // if (userExist) {
+        //     //     console.log(userExist);
+        //     // }
+        //     res.send("message sent")}).catch(error =>{
+        //     const err = new HttpError(error.message,405)
+        //     next(err);
+        // });
         
     } catch (error) {
         const err = new HttpError("Server Error try again later",405)
--- GMarket-server/app.js
+++ GMarket-server/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser")
 require("dotenv").config();
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
 
 app.use("/auth", AuthRoutes);
