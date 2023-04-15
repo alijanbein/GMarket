4ec355a1bb690b0b0b0bee74e513dc51f741c40d
AlINJFSw <alijanbeincs@gmail.com>
2023-04-15 14:54:38 +0300

    added twilo phone number send message

--- GMarket-server/app.js
+++ GMarket-server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
-
 const app = express()
+require("dotenv").config();
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
+client.messages
+.create({
+   body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+   from: '+16203776088',
+   to: '+96170838043'
+ })
+.then(message => console.log(message.sid));
 
-app.listen(4000,()=>{
-    console.log("started");
+app.listen(process.env.PORT,()=>{
 })
