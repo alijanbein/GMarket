fcd458a9b717c94d7a43d85a5154522ba4edacae
AlINJFSw <alijanbeincs@gmail.com>
2023-04-16 17:54:59 +0300

    added send message api

--- GMarket-server/routes/user-routes.js
+++ GMarket-server/routes/user-routes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const router = Router();
-const { getUserByNumber, getRate, addRate, reportUser, getReports, getUsersMessgesdBefore } = require("../Controllers/userController");
+const { getUserByNumber, getRate, addRate, reportUser, getReports, getUsersMessgesdBefore, sendMessage } = require("../Controllers/userController");
 
 router.post("/get_user_by_nummber",getUserByNumber);
 router.post("/get_rate",getRate);
@@ -8,5 +8,6 @@ router.post("/add_rate",addRate);
 router.post("/report_user",reportUser);
 router.post("/get_reports",getReports);
 router.post("/get_all_messaging_users",getUsersMessgesdBefore);
+router.post("/send_message",sendMessage);
 
 module.exports = router;
