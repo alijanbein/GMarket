78936460d9c9626dc96745fa4145b082fdbfcfa1
AlINJFSw <alijanbeincs@gmail.com>
2023-04-16 17:46:41 +0300

    added send message api

--- GMarket-server/Controllers/userController.js
+++ GMarket-server/Controllers/userController.js
@@ -1,3 +1,4 @@
+const Message = require("../Models/Message.model");
 const User = require("../Models/User.model");
 const HttpError = require("../support/http-error");
 
@@ -16,7 +17,7 @@ exports.getUserByNumber = async (req, res, next) => {
       return next(err);
     }
     res.send({
-      status: "succes",
+      status: "sucess",
       user: user,
     });
   } catch (error) {
@@ -34,27 +35,28 @@ exports.addRate = async (req, res, next) => {
       const err = new HttpError("User undefined", 405);
       return next(err);
     }
-    const ratedBefore = targetUser.rating.filter(data => data.user_number == user_number);
-    if(ratedBefore.length > 0){
-        ratedBefore[0].rate = rating;
-        await targetUser.save()
+    const ratedBefore = targetUser.rating.filter(
+      (data) => data.user_number == user_number
+    );
+    if (ratedBefore.length > 0) {
+      ratedBefore[0].rate = rating;
+      await targetUser.save();
+    } else {
+      const newRate = {
+        user_number: user_number,
+        rate: rating,
+      };
+      targetUser.rating.push(newRate);
+      await targetUser.save();
     }
-   else {
-    const newRate  = {
-        user_number : user_number,
-        rate: rating 
-    }
-    targetUser.rating.push(newRate);
-    await targetUser.save();
-   }
-    res.send({ status: "succes", user: targetUser });
+    res.send({ status: "sucess", user: targetUser });
   } catch (error) {
     const err = new HttpError(error.message, 500);
     return next(err);
   }
 };
 
-exports.getRate = async (req,res,next) => {
+exports.getRate = async (req, res, next) => {
   try {
     const { phone_number } = req.body;
     const user = await User.findOne({ phone_number: phone_number });
@@ -63,25 +65,21 @@ exports.getRate = async (req,res,next) => {
       return next(err);
     }
     const sum = user.rating.reduce((acc, curr) => acc + curr.rate, 0);
-    const rating  = sum/user.rating.length;
-    res.send({status: "succes",
-    rating: rating
-})
-
+    const rating = sum / user.rating.length;
+    res.send({ status: "sucess", rating: rating });
   } catch (error) {
     const err = new HttpError("Server Error", 500);
     return next(err);
   }
 };
 
-
 exports.reportUser = async (req, res, next) => {
   try {
     const user_number = req.user.phone_number;
     const { message, phone_number } = req.body;
-    if(message.length <= 0){
-        const err = new HttpError("Invalid input", 405);
-        return next(err);
+    if (message.length <= 0) {
+      const err = new HttpError("Invalid input", 405);
+      return next(err);
     }
     const targetUser = await User.findOne({ phone_number: phone_number });
     if (!targetUser) {
@@ -89,28 +87,32 @@ exports.reportUser = async (req, res, next) => {
       return next(err);
     }
     console.log(message);
-    const reportedBefore = targetUser.reported.filter(data => data.user_number == user_number);
+    const reportedBefore = targetUser.reported.filter(
+      (data) => data.user_number == user_number
+    );
     console.log(reportedBefore);
-    if(reportedBefore.length > 0){
-        const err = new HttpError("your report has been sent before waitng for admin", 200);
-        return next(err);
+    if (reportedBefore.length > 0) {
+      const err = new HttpError(
+        "your report has been sent before waitng for admin",
+        200
+      );
+      return next(err);
+    } else {
+      const newReport = {
+        user_number: user_number,
+        message: message,
+      };
+      targetUser.reported.push(newReport);
+      await targetUser.save();
     }
-   else {
-    const newReport  = {
-        user_number : user_number,
-        message: message 
-    }
-    targetUser.reported.push(newReport);
-    await targetUser.save();
-   }
-    res.send({ status: "succes", user: targetUser });
+    res.send({ status: "sucess", user: targetUser });
   } catch (error) {
     const err = new HttpError(error.message, 500);
     return next(err);
   }
 };
 
-exports.getReports = async (req,res,next) => {
+exports.getReports = async (req, res, next) => {
   try {
     const { phone_number } = req.body;
     const user = await User.findOne({ phone_number: phone_number });
@@ -118,23 +120,46 @@ exports.getReports = async (req,res,next) => {
       const err = new HttpError("User undefined", 405);
       return next(err);
     }
-    const reported  = user.reported;
-    res.send({status: "succes",
-    reported: reported
-})
-
+    const reported = user.reported;
+    res.send({ status: "sucess", reported: reported });
   } catch (error) {
     const err = new HttpError("Server Error", 500);
     return next(err);
   }
 };
 
-exports.sendMessage = async () => {
-    
-}
-exports.getUsersMessgesdBefore = async () => {}
-exports.getConversation = async () => {}
+exports.sendMessage = async (req,res,next) => {
+    const {message, reciver} = req.body;
+    if(!message || !reciver){
+        const err = new HttpError("invalid input", 4.5);
+    return next(err);
+    }
+    try {
+        const conversation = await Message.findOne({
+            participants:{$all: [sender,reciver]}
+        });
+        if(!conversation){
+            
+        }
+    } catch (error) {
+        
+    }
+};
 
-
-
+exports.getUsersMessgesdBefore = async (req, res, next) => {
+  const user = req.user;
+  const user_id = user._id;
+  try {
+    const messages = await Message.find({ participants: user_id })
+      .populate("participants")
+      .sort("-lastActivity");
+    res.send({
+        status: "sucess",
+        messages:messages
+    })
+  } catch (error) {
+    const err = new HttpError("Server Error", 500);
+    return next(err);
+  }
+};
 
--- GMarket-server/routes/user-routes.js
+++ GMarket-server/routes/user-routes.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const router = Router();
-const { getUserByNumber, getRate, addRate, reportUser, getReports } = require("../Controllers/userController");
+const { getUserByNumber, getRate, addRate, reportUser, getReports, getUsersMessgesdBefore } = require("../Controllers/userController");
 
 router.post("/get_user_by_nummber",getUserByNumber);
 router.post("/get_rate",getRate);
 router.post("/add_rate",addRate);
 router.post("/report_user",reportUser);
 router.post("/get_reports",getReports);
+router.post("/get_all_messaging_users",getUsersMessgesdBefore);
 
 module.exports = router;
