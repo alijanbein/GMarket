e59688f12f2dc3cf6e0beb6406fdcadcbdc00ed2
AlINJFSw <alijanbeincs@gmail.com>
2023-04-16 11:45:04 +0300

    added getUserBYNumber complete api

--- GMarket-server/Controllers/userController.js
+++ GMarket-server/Controllers/userController.js
@@ -1,3 +1,19 @@
-exports.getUserByNumber = async() => {
-    
+const User = require("../Models/User.module")
+
+exports.getUserByNumber = async(req,res,next) => {
+    try {
+        const {phone_number} = req.body 
+        const user = await User.findOne({phone_number:phone_number});
+        if (!user) {
+            const err = new HttpError("user does not exist", 401);
+            return next(err);
+        }
+        res.send({
+            status: "succes",
+            user: user
+        })
+    } catch (error) {
+        
+    }
 }
+
