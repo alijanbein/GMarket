4b2e925b9b1544225221b859ca0860cf33f21fbb
AlINJFSw <alijanbeincs@gmail.com>
2023-04-17 16:13:51 +0300

    added delete carousel router

--- GMarket-server/routes/admin-routes.js
+++ GMarket-server/routes/admin-routes.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
-const { getReports, getUsers, deleteUser, addCarouselImages } = require("../Controllers/adminController");
+const { getReports, getUsers, deleteUser, addCarouselImages, deleteCarousel } = require("../Controllers/adminController");
 const router = Router();
 
 router.get('/reports',getReports);
 router.get('/users',getUsers);
 router.post('/delete_by_id',deleteUser);
 router.post('/add_carousel_image',addCarouselImages);
+router.post('/delete_carousel_image',deleteCarousel);
 
 
 
