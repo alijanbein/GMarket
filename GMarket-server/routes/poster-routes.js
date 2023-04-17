69e460c1daf97ed648fc80560f93383168193fcc
AlINJFSw <alijanbeincs@gmail.com>
2023-04-17 14:52:13 +0300

    added delete poster routes

--- GMarket-server/routes/poster-routes.js
+++ GMarket-server/routes/poster-routes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const { addPoster, getRecommedePosts } = require("../Controllers/posterController");
+const { addPoster, getRecommedePosts, deletePoster } = require("../Controllers/posterController");
 const router = Router();
 
 router.post('/add_post',addPoster)
 router.post('/get_recommended_posters',getRecommedePosts)
+router.post('/delete_poster',deletePoster)
 
 module.exports = router
