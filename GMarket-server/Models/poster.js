53e1142baa2f5b4fd9c1147d061e1edd67826a8b
AlINJFSw <alijanbeincs@gmail.com>
2023-04-17 13:38:02 +0300

    updated poster model

--- GMarket-server/Models/poster.js
+++ GMarket-server/Models/poster.js
@@ -1,7 +1,10 @@
 const { default: mongoose } = require("mongoose");
 
 const posterSchema = new mongoose.Schema({
-
+    title : { type: String, require: true },
+    product_type:{ type: String, require: true },
+    farmer: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    description:  { type: String, require: true }
 })
 
 const Poster = mongoose.model("Poster",posterSchema);
