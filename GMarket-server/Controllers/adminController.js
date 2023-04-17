7fc29a42578195221d35061df898f88c7e4c64d8
AlINJFSw <alijanbeincs@gmail.com>
2023-04-17 12:25:20 +0300

    updated admin delete user

--- GMarket-server/Controllers/adminController.js
+++ GMarket-server/Controllers/adminController.js
@@ -3,57 +3,49 @@ const User = require("../Models/User.model");
 const HttpError = require("../support/http-error");
 
 exports.getReports = async (req, res, next) => {
-    try {
+  try {
     const reports = await Report.find();
-    if(!reports){
-        const err = new HttpError("can't find reports", 403);
+    if (!reports) {
+      const err = new HttpError("can't find reports", 403);
       return next(err);
     }
-      res.send({ status: "sucess", reports:reports });
-    } catch (error) {
-      const err = new HttpError("Server Error", 500);
-      return next(err);
-    }
-  };
-
-
-  exports.getUsers = async(req,res,next) => {
-    try {
-        const users = await User.find();
-        if(!users){
-            const err = new HttpError("can't find Users", 405);
-            return next(err);
-        }
-        res.send({
-            status: "sucess",
-            users: users
-        })
-    } catch (error) {
-        const err = new HttpError("Server Error", 500);
-        return next(err);
-    }
+    res.send({ status: "sucess", reports: reports });
+  } catch (error) {
+    const err = new HttpError("Server Error", 500);
+    return next(err);
   }
+};
 
-  exports.deleteUser = async(req,res,next) => {
-      try {
-        const {userId} = req.body
-        User.findOneAndDelete({_id:userId},(err,user) => {
-            if(err){
-                const err = new HttpError("Somthing wen't wrong", 500);
-                return next(err);
-            }
-            if(!user){
-                const err = new HttpError("user does not exist", 500);
-                return next(err);
-            }
-            else {
-                res.send({status: "sucess",
-                user:user
-            })
-            }
-        })
-      } catch (error) {
-        const err = new HttpError("Server Error", 500);
+exports.getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    if (!users) {
+      const err = new HttpError("can't find Users", 405);
+      return next(err);
+    }
+    res.send({
+      status: "sucess",
+      users: users,
+    });
+  } catch (error) {
+    const err = new HttpError("Server Error", 500);
+    return next(err);
+  }
+};
+
+exports.deleteUser = async (req, res, next) => {
+  try {
+    const { userId } = req.body;
+    User.findOneAndDelete({ _id: userId })
+      .then((deletetedUser) => {
+        res.send({ status: "sucess", user: deletetedUser });
+      })
+      .catch((error) => {
+        const err = new HttpError(error.message, 500);
         return next(err);
-      }
-    } 
+      });
+  } catch (error) {
+    const err = new HttpError(error.message, 500);
+    return next(err);
+  }
+};
