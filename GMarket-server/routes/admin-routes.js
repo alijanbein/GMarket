20986664b70f6e2bf52cde6ce6b831aab764dc8a
AlINJFSw <alijanbeincs@gmail.com>
2023-04-17 11:40:38 +0300

    added delete user by id routes

--- GMarket-server/routes/admin-routes.js
+++ GMarket-server/routes/admin-routes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
-const { getReports, getUsers } = require("../Controllers/adminController");
+const { getReports, getUsers, deleteUser } = require("../Controllers/adminController");
 const router = Router();
 
 router.get('/reports',getReports);
 router.get('/users',getUsers);
+router.get('/delete_by_id',deleteUser);
 
 
 
