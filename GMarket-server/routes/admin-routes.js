ac55f6f052419954d163bba2278b1b643689bd4f
AlINJFSw <alijanbeincs@gmail.com>
2023-04-17 11:31:46 +0300

    added get all users routes

--- GMarket-server/routes/admin-routes.js
+++ GMarket-server/routes/admin-routes.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
-const { getReports } = require("../Controllers/adminController");
+const { getReports, getUsers } = require("../Controllers/adminController");
 const router = Router();
 
 router.get('/reports',getReports);
+router.get('/users',getUsers);
+
 
 
 module.exports = router
