be33bbfb9998dcb075b5c77303e45e2e11e030e0
AlINJFSw <alijanbeincs@gmail.com>
2023-04-17 11:39:47 +0300

    added delete user by id

--- GMarket-server/Controllers/adminController.js
+++ GMarket-server/Controllers/adminController.js
@@ -22,7 +22,7 @@ exports.getReports = async (req, res, next) => {
     try {
         const users = await User.find();
         if(!users){
-            const err = new HttpError("can't find Users", 500);
+            const err = new HttpError("can't find Users", 405);
             return next(err);
         }
         res.send({
@@ -34,3 +34,27 @@ exports.getReports = async (req, res, next) => {
         return next(err);
     }
   }
+
+  exports.deleteUser = async(req,res,next) => {
+      try {
+        const {userId} = req.body
+        User.findOneAndDelete({_id:userId},(err,user) => {
+            if(err){
+                const err = new HttpError("Somthing wen't wrong", 500);
+                return next(err);
+            }
+            if(!user){
+                const err = new HttpError("user does not exist", 500);
+                return next(err);
+            }
+            else {
+                res.send({status: "sucess",
+                user:user
+            })
+            }
+        })
+      } catch (error) {
+        const err = new HttpError("Server Error", 500);
+        return next(err);
+      }
+    } 
