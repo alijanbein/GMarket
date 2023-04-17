e1893cb86e943a156d3dea1a71d9ed224cc3bb67
AlINJFSw <alijanbeincs@gmail.com>
2023-04-17 11:59:27 +0300

    updated admin middleware

--- GMarket-server/middlewares/adminMiddleware.js
+++ GMarket-server/middlewares/adminMiddleware.js
@@ -2,7 +2,7 @@ const HttpError = require("../support/http-error");
 
 exports.adminMiddleware = (req,res,next) => {
     console.log(req.user.type);
-    if(req.user.role == "admin") next();
+    if(req.user.type == "admin") next();
     else {
         const error = new HttpError("your are not an admin",405);
         return next(error)
