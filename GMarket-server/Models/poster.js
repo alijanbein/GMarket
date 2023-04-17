8133c9735e8956e4065a0a46f86c496a1fa101fb
AlINJFSw <alijanbeincs@gmail.com>
2023-04-17 13:27:14 +0300

    added poster Model

---
+++ GMarket-server/Models/poster.js
@@ -0,0 +1,9 @@
+const { default: mongoose } = require("mongoose");
+
+const posterSchema = new mongoose.Schema({
+
+})
+
+const Poster = mongoose.model("Poster",posterSchema);
+
+module.exports = Poster
