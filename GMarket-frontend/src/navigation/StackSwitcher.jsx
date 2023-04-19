4ec9179d0d73f440f7b82a2fc5476c787126fc17
AlINJFSw <alijanbeincs@gmail.com>
2023-04-20 00:13:38 +0300

    added redux files structure

---
+++ GMarket-frontend/src/navigation/StackSwitcher.jsx
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+
+export class StackSwitcher extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        
+      </NavigationContainer>
+    )
+  }
+}
+
+export default StackSwitcher
