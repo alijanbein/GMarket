5c8ccaa46f61a296effb80128a7cd5d0ee54468d
AlINJFSw <alijanbeincs@gmail.com>
2023-04-20 00:19:17 +0300

    added auth slice complee

---
+++ GMarket-frontend/src/redux/slices/authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    isLoggedin: false,
+    token: "",
+    userData : {}
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state) => {
+            state.isLoggedin = true
+        },
+        logout: (state) => {
+            state.isLoggedin = false
+        }
+    }
+})
+
+export const { login, logout} = authSlice.actions;
+
+export default authSlice.reducer;
