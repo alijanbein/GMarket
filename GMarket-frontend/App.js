0930125eb8fba220b932ff4639328da4c5ebdb90
AlINJFSw <alijanbeincs@gmail.com>
2023-04-20 00:23:38 +0300

    added store to the app

--- GMarket-frontend/App.js
+++ GMarket-frontend/App.js
@@ -1,9 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import {  StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
+import StackSwitcher from './src/navigation/StackSwitcher';
 
 export default function App() {
   return (
-    
+    <Provider store={store}>
+      <StackSwitcher/>
+    </Provider>
   );
 }
 
