4d6b71e7d8b1fe7171905621d118576241afb218
AlINJFSw <alijanbeincs@gmail.com>
2023-04-20 00:09:55 +0300

    added welcom screen start

---
+++ GMarket-frontend/src/screens/welcome/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './styles'
+
+const Welcome = () => {
+  return (
+    <View style = {styles.container}>
+      <Text>Welcome</Text>
+    </View>
+  )
+}
+
+export default Welcome
