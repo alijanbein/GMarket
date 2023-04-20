a14b506221ac00455e59226326be5cd4502cbed8
AlINJFSw <alijanbeincs@gmail.com>
2023-04-20 14:49:27 +0300

    added auth stack navigator

---
+++ GMarket-frontend/src/navigation/authSatack.js
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import AuthPhoneInputScreen from '../screens/authPhoneInputScreen'
+import Welcome from '../screens/welcome'
+
+const authSatack = () => {
+    const stack  = createStackNavigator()
+  return (
+   <stack.Navigator>
+        <stack.Screen name='welcome' component={Welcome} />
+        <stack.Screen name='auth_phone_input' component={AuthPhoneInputScreen} />
+   </stack.Navigator>
+  )
+}
+
+export default authSatack
