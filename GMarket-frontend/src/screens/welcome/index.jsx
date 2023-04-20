adc291ebf449ed7f9f2b5dd34f7682cb45bc6b1d
AlINJFSw <alijanbeincs@gmail.com>
2023-04-20 14:40:34 +0300

    added make the welcom screen trigger by user effect

--- GMarket-frontend/src/screens/welcome/index.jsx
+++ GMarket-frontend/src/screens/welcome/index.jsx
@@ -1,9 +1,12 @@
 import { View, Text, Image } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./styles";
 const logo = require("../../../assets/logo.png");
 
 const Welcome = () => {
+  useEffect(()=> {
+      
+  },[])
   return (
     <View style={styles.container}>
       <Image style = {styles.logo} source={logo} />
