ce1057f4c8788b1c1ad1976370e6b361f2db0429
AlINJFSw <alijanbeincs@gmail.com>
2023-04-20 22:05:59 +0300

    added code verification scren to navigation

--- GMarket-frontend/src/navigation/authSatack.js
+++ GMarket-frontend/src/navigation/authSatack.js
@@ -3,6 +3,7 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import AuthPhoneInputScreen from "../screens/authPhoneInputScreen";
 import Welcome from "../screens/welcome";
+import CodeVerificationScreen from "../screens/codeVerificationScreen";
 
 const AuthSatack = () => {
   const stack = createStackNavigator();
@@ -21,6 +22,10 @@ const AuthSatack = () => {
         }}
         name="auth_phone_input"
         component={AuthPhoneInputScreen}
+      />
+      <stack.Screen 
+        name="Code Verification"
+        component={CodeVerificationScreen}
       />
     </stack.Navigator>
   );
