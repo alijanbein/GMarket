4d444962b45a266269d36f69fa95d0e8a5d6778a
AlINJFSw <alijanbeincs@gmail.com>
2023-04-20 14:43:20 +0300

    added slice to welcome screen

--- GMarket-frontend/src/screens/welcome/index.jsx
+++ GMarket-frontend/src/screens/welcome/index.jsx
@@ -2,14 +2,17 @@ import { View, Text, Image } from "react-native";
 import React, { useEffect } from "react";
 import styles from "./styles";
 import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
 const logo = require("../../../assets/logo.png");
 
 const Welcome = () => {
+  const authSlice = useSelector(state => state.auth);
+
   const navigation = useNavigation()
   useEffect(()=> {
-      if(true){
+      if(!authSlice.isLoggedin){
           setTimeout(()=>{
-              
+
           },2000)
       }
 
