07d17561c93e7f2d8cdf1bd6262cff9e53a61440
AlINJFSw <alijanbeincs@gmail.com>
2023-04-20 14:36:37 +0300

    added backgroud to welcome screen 2

--- GMarket-frontend/src/screens/welcome/index.jsx
+++ GMarket-frontend/src/screens/welcome/index.jsx
@@ -1,13 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import styles from './styles'
+import { View, Text, Image } from "react-native";
+import React from "react";
+import styles from "./styles";
+const logo = require("../../../assets/logo.png");
 
 const Welcome = () => {
   return (
-    <View style = {styles.container}>
-      <Text>Welcome</Text>
+    <View style={styles.container}>
+      <Image style = {styles.logo} source={logo} />
     </View>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
--- GMarket-frontend/src/screens/welcome/styles.js
+++ GMarket-frontend/src/screens/welcome/styles.js
@@ -4,6 +4,13 @@ import { COLORS } from "../../contansts/colors";
 export default style = StyleSheet.create({
     container : {
         backgroundColor: COLORS.main,
-        flex: 1
+        flex: 1,
+        justifyContent: "center",
+        alignItems : "center"
+    },
+
+    logo: {
+        width : 600,
+        height : 400
     }
 })
