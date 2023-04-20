58b7374e8539134a22869701e6fdf6188a1b1b3c
AlINJFSw <alijanbeincs@gmail.com>
2023-04-20 14:41:52 +0300

    added navigation to welcom screen

--- GMarket-frontend/src/screens/welcome/index.jsx
+++ GMarket-frontend/src/screens/welcome/index.jsx
@@ -1,11 +1,18 @@
 import { View, Text, Image } from "react-native";
 import React, { useEffect } from "react";
 import styles from "./styles";
+import { useNavigation } from "@react-navigation/native";
 const logo = require("../../../assets/logo.png");
 
 const Welcome = () => {
+  const navigation = useNavigation()
   useEffect(()=> {
-      
+      if(true){
+          setTimeout(()=>{
+              
+          },2000)
+      }
+
   },[])
   return (
     <View style={styles.container}>
