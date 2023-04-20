849f8af8aeb7135bc8a46c829e3f7fad72e23191
AlINJFSw <alijanbeincs@gmail.com>
2023-04-20 21:56:46 +0300

    added code verification screen

---
+++ GMarket-frontend/src/screens/codeVerificationScreen/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { styles } from './style'
+
+const CodeVerificationScreen = () => {
+  return (
+    <View style = {styles.container}>
+            
+    </View>
+  )
+}
+
+export default CodeVerificationScreen
