d81e17295848909f8a29c100b42aef5344910278
AlINJFSw <alijanbeincs@gmail.com>
2023-04-21 17:00:44 +0300

    added complete profile info screen

---
+++ GMarket-frontend/src/screens/CompleteProfileScreen/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { styles } from './style'
+
+const CompleteProfileScrenn = () => {
+  return (
+    <View style = {styles.container}>
+      <Text>CompleteProfileScrenn</Text>
+    </View>
+  )
+}
+
+export default CompleteProfileScrenn
