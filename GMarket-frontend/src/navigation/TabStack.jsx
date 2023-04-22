363eadafe6d70b12020187e72cba4d2d628e733e
AlINJFSw <alijanbeincs@gmail.com>
2023-04-22 23:02:19 +0300

    added profile icon to buttonbar

--- GMarket-frontend/src/navigation/TabStack.jsx
+++ GMarket-frontend/src/navigation/TabStack.jsx
@@ -1,27 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import Welcome from '../screens/welcome'
-import ProfileScreen from '../screens/pofileScreen'
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { View, Text } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ProfileScreen from "../screens/pofileScreen";
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const TabStack = () => {
-    const Tabs = createBottomTabNavigator()
+  const Tabs = createBottomTabNavigator();
   return (
-   <Tabs.Navigator>
-       
-        <Tabs.Screen
-            options={{
-                tabBarIcon: () => {
-                    <Text>home</Text>
-                }
-            }}
-            name= "test"
-            component={ProfileScreen}
-        />
+    <Tabs.Navigator>
+      <Tabs.Screen
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: () => 
+            <Icon name="user" size={30} color="#000" />
+          
+        }}
+        name="Profile"
+        component={ProfileScreen}
+      />
+   
+    </Tabs.Navigator>
+  );
+};
 
-   </Tabs.Navigator>
-  )
-}
-
-export default TabStack
+export default TabStack;
