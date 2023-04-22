edaccbe2c8995deda9899704eeedd0070a4aec24
AlINJFSw <alijanbeincs@gmail.com>
2023-04-22 22:32:35 +0300

    added profileScren styles and connection to style

---
+++ GMarket-frontend/src/screens/pofileScreen/index.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native/types";
+
+export const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        backgroundColor: "yellow"
+    }
+})
---
+++ GMarket-frontend/src/screens/pofileScreen/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { styles } from '.'
+
+const ProfileScreen = () => {
+  return (
+    <View style = {styles.container}>
+      <Text>ProfileScreen</Text>
+    </View>
+  )
+}
+
+export default ProfileScreen
