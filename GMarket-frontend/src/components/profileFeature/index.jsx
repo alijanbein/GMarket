21334d2de7d333bb77037d8b3bb23826b79d88b5
AlINJFSw <alijanbeincs@gmail.com>
2023-04-23 12:05:26 +0300

    added props to feature

--- GMarket-frontend/src/components/profileFeature/index.jsx
+++ GMarket-frontend/src/components/profileFeature/index.jsx
@@ -3,11 +3,11 @@ import React from "react";
 import { styles } from "./style";
 import Icon from "react-native-vector-icons/FontAwesome";
 
-const ProfileFeature = () => {
+const ProfileFeature = (props) => {
   return (
-    <TouchableOpacity style={styles.container}>
-      <Icon name="credit-card" size={30} color="#000" />
-      <Text style = {styles.title}>Payment</Text>
+    <TouchableOpacity onPress={props.onPress} style={styles.container}>
+      <Icon name={props.icon} size={30} color="#000" />
+      <Text style = {styles.title}>{props.title}</Text>
     </TouchableOpacity>
   );
 };
