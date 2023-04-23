0c158316ae05dffc390a2b48c9b5250f66b8c752
AlINJFSw <alijanbeincs@gmail.com>
2023-04-23 20:37:31 +0300

    added search screen

---
+++ GMarket-frontend/src/screens/searchScreen/style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import { SPACING } from "../../contansts/spacing";
+
+export default style = StyleSheet.create({
+    container : {
+        flex: 1,
+        backgroundColor:"#fff",
+        paddingHorizontal:SPACING.paddingHorizontal,
+        
+    },
+
+})
