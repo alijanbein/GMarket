7c760203044f55e405c83a22919863c051118483
AlINJFSw <alijanbeincs@gmail.com>
2023-04-23 20:50:12 +0300

    added search bar to search screen

--- GMarket-frontend/src/navigation/styles.js
+++ GMarket-frontend/src/navigation/styles.js
@@ -2,5 +2,8 @@ import { StyleSheet } from "react-native";
 import { COLORS } from "../contansts/colors";
 
 export const style = StyleSheet.create({
-    
+    headerTitle : {
+        fontWeight: "bold",
+        color:COLORS.textColor
+    }
 })
--- GMarket-frontend/src/screens/searchScreen/index.jsx
+++ GMarket-frontend/src/screens/searchScreen/index.jsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import SearchBar from '../../components/searchBar'
+import styles from './style'
 
 const SearchScreen = () => {
   return (
-    <View>
-      <Text>SearchScreen</Text>
+    <View style = {styles.container}>
+        <SearchBar />
     </View>
   )
 }
