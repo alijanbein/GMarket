0d986777b3314ca795d0a5c3e4c2e4b59003a322
AlINJFSw <alijanbeincs@gmail.com>
2023-04-23 13:16:56 +0300

    Removed new padding

--- GMarket-frontend/src/components/profileFeature/index.jsx
+++ GMarket-frontend/src/components/profileFeature/index.jsx
@@ -6,7 +6,7 @@ import Icon from "react-native-vector-icons/FontAwesome";
 const ProfileFeature = (props) => {
   return (
     <TouchableOpacity onPress={props.onPress} style={styles.container}>
-      <Icon solid ={false} style={styles.icon} name={props.icon} size={30} color="#000" />
+      <Icon solid ={false} style={styles.icon} name={props.icon} size={25} color="#000" />
       <Text style = {styles.title}>{props.title}</Text>
     </TouchableOpacity>
   );
--- GMarket-frontend/src/screens/pofileScreen/style.js
+++ GMarket-frontend/src/screens/pofileScreen/style.js
@@ -8,6 +8,7 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     paddingHorizontal:SPACING.paddingHorizontal,
+    paddingVertical:20
   },
   name: {
     fontSize: 25,
