7e9cb05a869338af6aaef3d75406d8e752034ee3
AlINJFSw <alijanbeincs@gmail.com>
2023-04-23 12:04:12 +0300

    added style for profileFeature complete

--- GMarket-frontend/src/components/profileFeature/index.jsx
+++ GMarket-frontend/src/components/profileFeature/index.jsx
@@ -1,13 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { styles } from './style'
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { styles } from "./style";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const ProfileFeature = () => {
   return (
-    <View style={styles.container}>
-      <Text>ProfileFeature</Text>
-    </View>
-  )
-}
+    <TouchableOpacity style={styles.container}>
+      <Icon name="credit-card" size={30} color="#000" />
+      <Text style = {styles.title}>Payment</Text>
+    </TouchableOpacity>
+  );
+};
 
-export default ProfileFeature
+export default ProfileFeature;
--- GMarket-frontend/src/components/profileFeature/style.js
+++ GMarket-frontend/src/components/profileFeature/style.js
@@ -2,11 +2,14 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    backgroundColor: "yellow",
     width: "100%",
     height: 60,
     marginTop: 15,
     alignItems: "center",
     flexDirection: "row",
   },
+  title: {
+    fontSize: 15,
+    marginLeft: 10
+  }
 });
