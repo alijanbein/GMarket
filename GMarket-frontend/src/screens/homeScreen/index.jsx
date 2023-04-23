d2248fb502e2b2f0f9b97ae16ed41ef5f468e156
AlINJFSw <alijanbeincs@gmail.com>
2023-04-23 17:53:53 +0300

    added home screen

---
+++ GMarket-frontend/src/screens/homeScreen/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { styles } from './style'
+
+const HomeScreen = () => {
+  return (
+    <View style ={styles.container}>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
---
+++ GMarket-frontend/src/screens/homeScreen/style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import { SPACING } from "../../contansts/spacing";
+
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal:SPACING.paddingHorizontal,
+    paddingVertical:20
+  },
+
+});
