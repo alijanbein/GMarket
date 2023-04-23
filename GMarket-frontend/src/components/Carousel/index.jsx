2804836c51985a55c48368c24cba7164cdaa3b1e
AlINJFSw <alijanbeincs@gmail.com>
2023-04-23 22:17:39 +0300

    fixed some issue

--- GMarket-frontend/src/components/Carousel/index.jsx
+++ GMarket-frontend/src/components/Carousel/index.jsx
@@ -1,50 +1,43 @@
-import React, { useState } from 'react';
-import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
-import Carousel from 'react-native-snap-carousel';
-import PropTypes from 'prop-types';
-import styles from './style';
-import { ViewPropTypes } from 'deprecated-react-native-prop-types';
+import React, { useState } from "react";
+import { View, Text, Image, StyleSheet, Dimensions } from "react-native";
+import Carousel from "react-native-snap-carousel";
+import PropTypes from "prop-types";
+import styles from "./style";
+import { ViewPropTypes } from "deprecated-react-native-prop-types";
 
-const { width: screenWidth } = Dimensions.get('window');
+const { width: screenWidth } = Dimensions.get("window");
 
 const images = [
   {
-    uri: 'https://picsum.photos/id/1003/500/500',
-    title: 'Image 1'
+    uri: "https://picsum.photos/id/1003/500/500",
+    title: "Image 1",
   },
   {
-    uri: 'https://picsum.photos/id/1004/500/500',
-    title: 'Image 2'
+    uri: "https://picsum.photos/id/1004/500/500",
+    title: "Image 2",
   },
   {
-    uri: 'https://picsum.photos/id/1005/500/500',
-    title: 'Image 3'
+    uri: "https://picsum.photos/id/1005/500/500",
+    title: "Image 3",
   },
   {
-    uri: 'https://picsum.photos/id/1006/500/500',
-    title: 'Image 4'
+    uri: "https://picsum.photos/id/1006/500/500",
+    title: "Image 4",
   },
 ];
 
+const renderImageItem = ({ item }) => {
+  return (
+    <View style={styles.imageContainer}>
+      <Image style={styles.image} source={{ uri: item.uri }} />
+    </View>
+  );
+};
 const CarouselScreen = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const renderImageItem = ({ item }) => {
-    return (
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={{ uri: item.uri }} />
-      </View>
-    );
-  };
-
-  renderImageItem.prototypes = {
-    item:PropTypes.shape({
-        uri :PropTypes.string
-    })
-  }
-   return (
+  return (
     <View style={styles.container}>
       <Carousel
+        layout={"default"}
         data={images}
         autoplay={true}
         autoplayInterval={4000}
@@ -52,15 +45,9 @@ const CarouselScreen = () => {
         sliderWidth={screenWidth}
         itemWidth={screenWidth - 50}
         loop={true}
-        onSnapToItem={(index) => setActiveIndex(index)}
       />
     </View>
   );
-
 };
-
-
-
-
 
 export default CarouselScreen;
