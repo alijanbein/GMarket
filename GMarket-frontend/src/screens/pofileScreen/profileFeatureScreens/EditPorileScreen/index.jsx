61b484f8a2e97f7d7a8e597e30510032ec4a5374
AlINJFSw <alijanbeincs@gmail.com>
2023-04-23 13:21:58 +0300

    added edit profile screen startup

---
+++ GMarket-frontend/src/screens/pofileScreen/profileFeatureScreens/EditPorileScreen/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { styles } from './style'
+
+const EditProfileScreen = () => {
+  return (
+    <View style ={styles.container}>
+      <Text>EditProfileScreen</Text>
+    </View>
+  )
+}
+
+export default EditProfileScreen
---
+++ GMarket-frontend/src/screens/pofileScreen/profileFeatureScreens/EditPorileScreen/style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "../../contansts/colors";
+import { SPACING } from "../../contansts/spacing";
+import { FONTS } from "../../contansts/fonts";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    paddingHorizontal:SPACING.paddingHorizontal,
+    paddingVertical:20
+  
+}});
