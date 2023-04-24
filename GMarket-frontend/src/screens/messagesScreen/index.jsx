8e4fac53c7911a62f07d0bc637630f300442d35d
AlINJFSw <alijanbeincs@gmail.com>
2023-04-24 16:43:58 +0300

    added messages screen

---
+++ GMarket-frontend/src/screens/messagesScreen/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { styles } from './style'
+
+const MessagesScreen = () => {
+  return (
+    <View style = {styles.container}>
+      <Text>MessagesScreen</Text>
+    </View>
+  )
+}
+
+export default MessagesScreen
---
+++ GMarket-frontend/src/screens/messagesScreen/style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+import { SPACING } from "../../contansts/spacing";
+import { COLORS } from "../../contansts/colors";
+
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "yellow",
+    paddingHorizontal:SPACING.paddingHorizontal,
+    paddingVertical:20,
+  },
+
+});
