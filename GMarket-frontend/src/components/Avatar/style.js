c4f2fa5d595ea77a0f0e1378850817f891a7ce16
AlINJFSw <alijanbeincs@gmail.com>
2023-04-24 17:17:46 +0300

    added avatar component new style

--- GMarket-frontend/src/components/Avatar/style.js
+++ GMarket-frontend/src/components/Avatar/style.js
@@ -3,6 +3,7 @@ import { StyleSheet } from "react-native";
 export default styles = StyleSheet.create({
     container: {
         width:"100%",
-        height: "100%"
+        height: "100%",
+        borderRadius:30
     }
 })
