b5d775eaeef362374e27ab4cfca8a318fbe3fd5c
AlINJFSw <alijanbeincs@gmail.com>
2023-04-24 18:59:34 +0300

    added conversation screen

---
+++ GMarket-frontend/src/screens/conversationScreen/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { styles } from './style'
+
+const ConversationScreen = () => {
+  return (
+    <View style ={styles.container}>
+      <Text>ConversationScreen</Text>
+    </View>
+  )
+}
+
+export default ConversationScreen
