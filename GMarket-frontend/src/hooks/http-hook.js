4ea79fb743a9c5567ee86616021e751681a762bf
AlINJFSw <alijanbeincs@gmail.com>
2023-04-26 09:24:18 +0300

    updated useHttp hook

--- GMarket-frontend/src/hooks/http-hook.js
+++ GMarket-frontend/src/hooks/http-hook.js
@@ -1,22 +1,32 @@
-const UseHttp = async (url, method = "GET", body = "", header = {}) => {
-  let data;
-  const URL = "http://localhost:3000/";
-  try {
-    const Response =
-      (await fetch(URL + url, {
-        method: method,
-        headers: header,
-        body: !!body ? JSON.stringify(body) : null,
-      })) || null;
-    data = await Response.json();
-    if (!Response.ok) {
-      throw new Error(data);
+import { useState } from "react";
+import { BASE_URL } from "../contansts/spacing";
+
+const UseHttp = () => {
+  const [isLoading,setIsLoading] = useState(false)
+  const [error,setIsError] = useState('')
+  const sendRequest = async (url, method = "GET", body = "", header = {}) => {
+    let data;
+    try {
+      setIsError('')
+      setIsLoading(true)
+      const Response =
+        (await fetch(BASE_URL + url, {
+          method: method,
+          headers: header,
+          body: !!body ? JSON.stringify(body) : null,
+        })) || null;
+      data = await Response.json();
+      if (!Response.ok) {
+        throw new Error(data);
+      }
+      setIsLoading(false)
+    } catch (err) {
+      console.log(err.message);
+      setIsLoading(false)
+      setIsError(error.message)
     }
-  } catch (err) {
-    console.log(err.message);
-  }
 
-  return data;
+    return data;
+  };
 };
-
 export default UseHttp;
