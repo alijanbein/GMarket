7e70056bd04c132fa5d5bcf948ea36b5490a0357
AlINJFSw <alijanbeincs@gmail.com>
2023-04-26 20:46:03 +0300

    edited show model

--- GMarket-frontend/src/navigation/authSatack.js
+++ GMarket-frontend/src/navigation/authSatack.js
@@ -19,7 +19,11 @@ const AuthSatack = () => {
         name="welcome"
         component={Welcome}
       /> */}
-     
+      <stack.Screen
+        options={Tstyles.authOption}
+        name="Code Verification"
+        component={CodeVerificationScreen}
+      />
       <stack.Screen
         options={{
           headerShown: false,
@@ -27,11 +31,7 @@ const AuthSatack = () => {
         name="auth_phone_input"
         component={AuthPhoneInputScreen}
       />
-       <stack.Screen
-        options={Tstyles.authOption}
-        name="Code Verification"
-        component={CodeVerificationScreen}
-      />
+      
       
       <stack.Screen
         options={Tstyles.authOption}
