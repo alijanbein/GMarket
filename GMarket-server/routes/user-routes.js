119219588de9730a4b06e45783fcfd5e10dc5e5b
AlINJFSw <alijanbeincs@gmail.com>
2023-04-27 16:34:39 +0300

    added upate user api  to router

--- GMarket-server/routes/user-routes.js
+++ GMarket-server/routes/user-routes.js
@@ -1,15 +1,28 @@
 const { Router } = require("express");
 const router = Router();
-const { getUserByNumber, getRate, addRate, reportUser, getReports, getUsersMessgesdBefore, sendMessage, getOneConversation, search, getCarouselImages } = require("../Controllers/userController");
+const {
+  getUserByNumber,
+  getRate,
+  addRate,
+  reportUser,
+  getReports,
+  getUsersMessgesdBefore,
+  sendMessage,
+  getOneConversation,
+  search,
+  getCarouselImages,
+  updateUserData,
+} = require("../Controllers/userController");
 
-router.post("/get_user_by_nummber",getUserByNumber);
-router.post("/get_rate",getRate);
-router.post("/add_rate",addRate);
-router.post("/report_user",reportUser);
-router.post("/get_all_messaging_users",getUsersMessgesdBefore);
-router.post("/send_message",sendMessage);
-router.post("/get_one_conversation",getOneConversation);
-router.post("/search",search);
-router.get("/get_carousel_images",getCarouselImages);
+router.post("/get_user_by_nummber", getUserByNumber);
+router.post("/get_rate", getRate);
+router.post("/add_rate", addRate);
+router.post("/report_user", reportUser);
+router.post("/get_all_messaging_users", getUsersMessgesdBefore);
+router.post("/send_message", sendMessage);
+router.post("/get_one_conversation", getOneConversation);
+router.post("/search", search);
+router.get("/get_carousel_images", getCarouselImages);
+router.get("/update_user_data", updateUserData);
 
 module.exports = router;
