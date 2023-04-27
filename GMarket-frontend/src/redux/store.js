60ef7cf04c3c3c9a70973e412d7f1be6826d1126
AlINJFSw <alijanbeincs@gmail.com>
2023-04-27 22:37:06 +0300

    frontend added current slice to store

--- GMarket-frontend/src/redux/store.js
+++ GMarket-frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import authSlice, {authReducer} from "./slices/authSlice"
 import appDataSlice from "./slices/appDataSlice"
+import currentSlice from "./slices/currentSlice"
 const reducer = {
     auth : authSlice,
-    app: appDataSlice
+    app: appDataSlice,
+    current:currentSlice
 }
 
 export const store = configureStore({
