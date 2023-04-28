056e1351b639efed2377683e0ccf3454c58f263e
AlINJFSw <alijanbeincs@gmail.com>
2023-04-28 13:50:25 +0300

    frontend added auction Screen

---
+++ GMarket-frontend/src/screens/auctionScreen/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AuctionScreen = () => {
+  return (
+    <View>
+      <Text>AuctionScreen</Text>
+    </View>
+  )
+}
+
+export default AuctionScreen
