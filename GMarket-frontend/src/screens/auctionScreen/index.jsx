77b59b7660572702e837c93e9a58a8eb77cde442
AlINJFSw <alijanbeincs@gmail.com>
2023-04-28 13:51:22 +0300

    frontend added acution style class

--- GMarket-frontend/src/screens/auctionScreen/index.jsx
+++ GMarket-frontend/src/screens/auctionScreen/index.jsx
@@ -1,9 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import style from './style'
 
 const AuctionScreen = () => {
   return (
-    <View>
+    <View style = {style.container}>
       <Text>AuctionScreen</Text>
     </View>
   )
---
+++ GMarket-frontend/src/screens/auctionScreen/style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "../../contansts/colors";
+import { FONTS } from "../../contansts/fonts";
+import { SPACING } from "../../contansts/spacing";
+
+export default styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal:SPACING.paddingHorizontal,
+    paddingVertical:20,
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+
+});
