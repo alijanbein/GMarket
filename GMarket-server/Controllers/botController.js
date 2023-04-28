d798931d07c7053440011dffff742e04013c3d6e
AlINJFSw <alijanbeincs@gmail.com>
2023-04-28 11:30:40 +0300

    backend added google servicess sensetive data to env file

--- GMarket-server/Controllers/botController.js
+++ GMarket-server/Controllers/botController.js
@@ -1,7 +1,11 @@
 const dialogflow = require('dialogflow');
 
-
 exports.sendMessageToBot = async(req,res,next) => {
+    // const googleProjectId = process.env.googleProjectId
+    // const googlePrivateKeyId = process.env.googlePrivateKeyId
+    // const googlePrivateKey = process.env.googlePrivateKey
+    // const googleClientEmail = process.env.googleClientEmail
+    console.log(googlePrivateKeyId);
     const {message,sessionId} = req.body;
     console.log({
         message,
