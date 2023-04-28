f5fa28f3975668dac0905d1a1833ea29277829a6
AlINJFSw <alijanbeincs@gmail.com>
2023-04-28 11:10:10 +0300

    backend added dialogue flow library

--- GMarket-server/Controllers/botController.js
+++ GMarket-server/Controllers/botController.js
@@ -1,3 +1,10 @@
+const dialogflow = require('dialogflow');
+
+
 exports.sendMessageToBot = async(req,res,next) => {
-    
+    const {message,sessionId} = req.body;
+    console.log({
+        message,
+        sessionId
+    });
 }
