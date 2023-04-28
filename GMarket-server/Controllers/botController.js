d7141b2aa37ef94ee4c6e04d54b0c6589b292568
AlINJFSw <alijanbeincs@gmail.com>
2023-04-28 11:54:41 +0300

    backend connected the chatbot to the backend

--- GMarket-server/Controllers/botController.js
+++ GMarket-server/Controllers/botController.js
@@ -1,14 +1,40 @@
 const dialogflow = require('dialogflow');
 
 exports.sendMessageToBot = async(req,res,next) => {
-    // const googleProjectId = process.env.googleProjectId
-    // const googlePrivateKeyId = process.env.googlePrivateKeyId
-    // const googlePrivateKey = process.env.googlePrivateKey
-    // const googleClientEmail = process.env.googleClientEmail
-    console.log(googlePrivateKeyId);
+    const googleProjectId = process.env.googleProjectId
+    const googlePrivateKeyId = process.env.googlePrivateKeyId
+    const googlePrivateKey = process.env.googlePrivateKey
+    const googleClientEmail = process.env.googleClientEmail
+    const dialogFlowSessionLanguageCode = process.env.dialogFlowSessionLanguageCode
     const {message,sessionId} = req.body;
-    console.log({
-        message,
-        sessionId
-    });
+    const credentials  = {
+        client_email:googleClientEmail,
+        private_key:googlePrivateKey
+    }
+    const sessionClient = new dialogflow.SessionsClient({googleProjectId,credentials});
+    // const sessionPath = sessionClient.sessionPath(googleProjectId,sessionId)
+    const sessionPath = sessionClient.sessionPath(googleProjectId, sessionId);
+
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text:message,
+                languageCode:dialogFlowSessionLanguageCode
+            }
+        }
+    }
+    try {
+        const response = await sessionClient.detectIntent(request)
+        res.send({
+            status:"sucess",
+            response
+        })
+    } catch (error) {
+        res.send({
+            status:"error",
+           error:  error.message
+        })
+    }
+    
 }
