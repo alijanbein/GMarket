bc4e60f503ef7bc75e5648ce1794ce23c5477f63
AlINJFSw <alijanbeincs@gmail.com>
2023-05-02 16:53:47 +0300

    frontend :added context provieder to app

--- gmarket-admin-frontend/src/App.js
+++ gmarket-admin-frontend/src/App.js
@@ -1,11 +1,35 @@
-import {Routes,BrowserRouter,Route} from "react-router-dom"
+import { Routes, BrowserRouter, Route } from "react-router-dom";
 import AuthPage from "./pages/authPage";
+import { useEffect, useState } from "react";
+import AuthContext from "./context/auth-context";
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState("");
+
+  const login = (token) => {
+    localStorage.setItem("token", token);
+    setToken(token);
+    setIsLoggedIn(true);
+  };
+
+  const logout = () => {
+    localStorage.clear();
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    login(token);
+  }, []);
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/auth" element ={<AuthPage/>} />
-      </Routes>
+      <AuthContext.Provider value={{ isLoggedIn, token, login, logout }}>
+        <Routes>
+          <Route path="/auth" element={<AuthPage />} />
+        </Routes>
+      </AuthContext.Provider>
     </BrowserRouter>
   );
 }
